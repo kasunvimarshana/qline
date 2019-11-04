5744627d0a2ba755daca1415c5ee00c688df8ec2
Kasun Vimarshana <KasunV@brandix.com>
2019-11-04 09:31:31 +0530

    add test

---
+++ tests/shape_button/index(1).php
@@ -0,0 +1,48 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div id="home_quicklinks">
+                                <a class="quicklink link1" href="#">
+                                    <span class="ql_caption">
+                                        <span class="outer">
+                                            <span class="inner">
+                                                <h2>Member fdf dfdsfsfds fdsgdsgdg gdsgdsgdgds dsgdsg cdsvds dsvdsvdvd vdsvdsvv dsvds</h2>
+                                            </span>
+                                        </span>
+                                    </span>
+                                    <span class="ql_top"></span>
+                                    <span class="ql_bottom"></span>
+                                </a>
+
+                                <a class="quicklink link2" href="#">
+                                    <span class="ql_caption">
+                                        <span class="outer">
+                                            <span class="inner">
+                                                <h2>Event</h2>
+                                            </span>
+                                        </span>
+                                    </span>
+                                    <span class="ql_top"></span>
+                                    <span class="ql_bottom"></span>
+                                </a>
+
+                                <a class="quicklink link3" href="#">
+                                    <span class="ql_caption">
+                                        <span class="outer">
+                                            <span class="inner">
+                                                <h2>Gallery </h2>
+                                            </span>
+                                        </span>
+                                    </span>
+                                    <span class="ql_top"></span>
+                                    <span class="ql_bottom"></span>
+                                </a>
+
+                                <div class="clear"></div>
+                            </div>
+                            <div class="hexagon">
+  <div class="hexTop"></div>
+  <div class="hexBottom"></div>
+</div>
---
+++ tests/toggle_button/index(1).php
@@ -0,0 +1,35 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
+
+<div class="container">
+	<div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="material-button-anim">
+          <ul class="list-inline" id="options">
+            <li class="option">
+              <button class="material-button option1" type="button">
+                <span class="fa fa-phone" aria-hidden="true"></span>
+              </button>
+            </li>
+            <li class="option">
+              <button class="material-button option2" type="button">
+                <span class="fa fa-envelope-o" aria-hidden="true"></span>
+              </button>
+            </li>
+            <li class="option">
+              <button class="material-button option3" type="button">
+                <span class="fa fa-pencil" aria-hidden="true"></span>
+              </button>
+            </li>
+          </ul>
+          <button class="material-button material-button-toggle" type="button">
+            <span class="fa fa-plus" aria-hidden="true"></span>
+          </button>
+        </div>
+      </div>
+	</div>
+</div>
